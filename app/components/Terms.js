3c46401198805b57403f1a5bea04b0d6096c43be
Artem Loban <tymanrock@gmail.com>
2020-11-11 23:52:08 +0200

    '

--- app/components/Terms.js
+++ app/components/Terms.js
@@ -1,31 +1,23 @@
 import React from 'react';
+import Page from './Page';
 
 function Terms() {
     return (
-        <div className="container container--narrow py-md-5">
+        <Page title="Terms and Cond">
             <h2>Our Terms &amp; Conditions</h2>
-            <p className="lead text-muted">Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Corporis dolorum labore quisquam
-                vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error
-                veritatis consequuntur, excepturi cumque fuga eum incidunt
+            <p className="lead text-muted">Lorem ipsum dolor sit ametconsectetur adipisicing elit. Corporis dolorum labore quisquam
+                vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid errorveritatis consequuntur, excepturi cumque fuga eum incidunt
                 doloremque?</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui
-                enim rem totam voluptatum. Aut saepe temporibus, facilis ex a
-                iste expedita minima dolorum dicta doloribus libero aliquid,
-                quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Fugiat suscipit beatae eum, est soluta ducimus ratione et
-                impedit sapiente, nihil, atque dignissimos adipisci? Totam atque
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At quienim rem totam voluptatum. Aut saepe temporibus, facilis ex a
+                iste expedita minima dolorum dicta doloribus libero aliquid,quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Fugiat suscipit beatae eum, est soluta ducimus ratione etimpedit sapiente, nihil, atque dignissimos adipisci? Totam atque
                 officia quis voluptates sed veniam?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Expedita voluptates quisquam possimus tenetur, dicta enim rerum
-                quis, quaerat id nobis provident quo dolorum sapiente temporibus
-                facere non repellendus consequatur cupiditate!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.Expedita voluptates quisquam possimus tenetur, dicta enim rerum
+                quis, quaerat id nobis provident quo dolorum sapiente temporibusfacere non repellendus consequatur cupiditate!</p>
             <h3>Details</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                laboriosam distinctio atque sint earum? Temporibus, voluptas
-                aspernatur aliquam nisi sed harum laborum, nemo odio animi
-                officia quisquam. Veniam, natus reprehenderit.</p>
-        </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaelaboriosam distinctio atque sint earum? Temporibus, voluptas
+                aspernatur aliquam nisi sed harum laborum, nemo odio animiofficia quisquam. Veniam, natus reprehenderit.</p>
+        </Page>
     )
 }
 
