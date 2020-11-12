ec821ebd59cdb22aded7e9514e72ffb84f9afd5b
Artem Loban <tymanrock@gmail.com>
2020-11-13 00:02:39 +0200

    Working With a Real Back-End

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import HeaderLoggedOut from './HeaderLoggedOut';
 import {Link} from 'react-router-dom';
 
 function Header() {
@@ -8,19 +9,7 @@ function Header() {
                 <h4 className="my-0 mr-md-auto font-weight-normal">
                     <Link to="/" className="text-white">ComplexApp</Link>
                 </h4>
-                <form className="mb-0 pt-2 pt-md-0">
-                    <div className="row align-items-center">
-                        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                            <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off"/>
-                        </div>
-                        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                            <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password"/>
-                        </div>
-                        <div className="col-md-auto">
-                            <button className="btn btn-success btn-sm">Sign In</button>
-                        </div>
-                    </div>
-                </form>
+                <HeaderLoggedOut />
             </div>
         </header>
     )
